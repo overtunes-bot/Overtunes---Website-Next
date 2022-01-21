03ba3fa0bd593f98616e085744b5e2672de020c1
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-01-21 09:43:19 +0700

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 text-center">
             <div className="p-4 sm:w-1/4 w-1/2 border-[#3C4652] border-2">
-              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-200">150K+</h2>
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-200">180K+</h2>
               <p className="leading-relaxed">Users</p>
             </div>
             <div className="p-4 sm:w-1/4 w-1/2 border-[#3C4652] border-2">
