c415687f362ae35bff30602d27c2d149366550f8
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-03-05 19:14:10 +0700

    feat: udpate footer

--- components/footer.js
+++ components/footer.js
@@ -2,29 +2,36 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className='border-[#3C4652] border-2 bg-[#04182E]'>
-            <div className='p-10 flex flex-col md:flex-row gap-4'>
-                <div className='border-[#3C4652] border-2 p-2 md:p-6 lg:p-10'>
-                    <h2 className='text-gray-200 font-bold text-2xl'>Overtunes</h2>
-                    <p className='text-gray-300/80 mt-2'>The Best Music Bot For Discord</p>
-                </div>
-                <div className='border-[#3C4652] uppercase border-2 flex gap-3 p-2 grow md:text-xl lg:text-2xl'>
-                    <div className='border-[#3C4652] border-2 p-2 md:p-6 md:gap-5 flex flex-col gap-2 grow'>
-                        <h3 className='text-gray-200 font-semibold'>Social</h3>
-                        <a href='https://discord.gg/hM8U8cHtwu' rel='noopener noreferrer' target={'_blank'} className='text-gray-200/70 hover:text-gray-200'>Discord</a>
-                        <a href='https://github.com/overtunes-bot' rel='noopener noreferrer' target={'_blank'} className='text-gray-200/70 hover:text-gray-200'>Github</a>
+        <footer className='bg-[#4696e6]'>
+            <div className='py-12 px-10 flex flex-col gap-4 mx-auto'>
+                <div className="flex flex-col gap-y-5 md:flex-row">
+                    <div className='flex-grow'>
+                        <h2 className='text-gray-200 font-bold text-2xl md:text-4xl'>Overtunes</h2>
+                        <p className='text-gray-300 mt-2 md:text-lg'>The Best Music Bot For Discord</p>
                     </div>
-                    <div className='border-[#3C4652] border-2 p-2 md:p-6 md:gap-5 flex flex-col gap-2 grow'>
-                        <h3 className='text-gray-200 font-semibold'>Content</h3>
-                        <Link href={'/commands'} passHref><p className='text-gray-200/70 cursor-pointer hover:text-gray-200'>Commands</p></Link>
-                        <a href='https://discord.gg/hM8U8cHtwu' rel='noopener noreferrer' target={'_blank'} className='text-gray-200/70 hover:text-gray-200'>Support</a>
-                        <Link href={'/blog'} passHref><p className='text-gray-200/70 cursor-pointer hover:text-gray-200'>Blog</p></Link>
+                    <div className='uppercase flex flex-col md:flex-row gap-3 md:gap-0 grow md:text-xl lg:text-2xl'>
+                        <div className='md:gap-5 flex flex-col gap-2 grow'>
+                            <h3 className='text-gray-200 font-bold text-xl'>Social</h3>
+                            <a href='https://discord.gg/hM8U8cHtwu' rel='noopener noreferrer' target={'_blank'} className='text-gray-200 hover:text-gray-200'>Discord</a>
+                            <a href='https://github.com/overtunes-bot' rel='noopener noreferrer' target={'_blank'} className='text-gray-200 hover:text-gray-200'>Github</a>
+                        </div>
+                        <div className='md:gap-5 flex flex-col gap-2 grow'>
+                            <h3 className='text-gray-200 font-bold text-xl'>Content</h3>
+                            <Link href={'/commands'} passHref><p className='text-gray-200 cursor-pointer hover:text-gray-200'>Commands</p></Link>
+                            <a href='https://discord.gg/hM8U8cHtwu' rel='noopener noreferrer' target={'_blank'} className='text-gray-200 hover:text-gray-200'>Support</a>
+                            <Link href={'/blog'} passHref><p className='text-gray-200 cursor-pointer hover:text-gray-200'>Blog</p></Link>
+                        </div>
+                        <div className='md:gap-5 flex flex-col gap-2 grow'>
+                            <h3 className='text-gray-200 font-bold text-xl'>Legal</h3>
+                            <Link href={'/privacy'} passHref><p className='text-gray-200 cursor-pointer hover:text-gray-200'>Privacy Policy</p></Link>
+                            <Link href={'/tos'} passHref><p className='text-gray-200 cursor-pointer hover:text-gray-200'>Term Of Service</p></Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='p-2 md:p-6 flex justify-between items-center'>
-                <p className='text-gray-300 text-[10px] md:text-xl'>© Overtunes {new Date().getFullYear()} - All rights reserved.</p>
-                <p className='text-right font-mono text-[12px] md:text-xl text-gray-300'>Made with 💖 by Vann#3784</p>
+                <div className='flex flex-col'>
+                    <p className='text-gray-300 md:text-lg'>© Overtunes {new Date().getFullYear()} - All rights reserved.</p>
+                    <p className='md:text-lg text-gray-300'>Made with 💖 by Vann#3784</p>
+                </div>
             </div>
         </footer>
     )
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html>
+        <Html className='scroll-smooth'>
             <Head>
                 <script data-ad-client="ca-pub-9042235887207452" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
