28d1d142909a5def04bc7e1b1b09d0314fb1c687
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-03-06 16:45:55 +0700

    feat: adsense

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
             <Head>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2622884275844647"
                     crossOrigin="anonymous"></script>
+                <script async custom-element="amp-auto-ads"
+                    src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+                </script>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet" />
@@ -13,6 +16,9 @@ export default function Document() {
                 <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
             </Head>
             <body className='bg-[#36393F]'>
+                <amp-auto-ads type="adsense"
+                    data-ad-client="ca-pub-2622884275844647">
+                </amp-auto-ads>
                 <Main />
                 <NextScript />
             </body>
