4d8197dc00e2a335b6c57dd6cbf96cc3095c603e
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-03-08 07:34:48 +0700

    secret auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import DiscordProvider from "next-auth/providers/discord"
 export default NextAuth({
     providers: [
         DiscordProvider({
-            clientId: '862875722339319828',
-            clientSecret: '9Dr7Ct2zwOVtWTZo_U0T128KwOOXCw7g',
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
         })
     ],
+    secret: process.env.NEXT_AUTH_SECRET,
 })
