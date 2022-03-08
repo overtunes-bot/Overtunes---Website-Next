c6f893377676cfc95a2aa92519027a997f74c100
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-03-08 08:20:43 +0700

    endpoint

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,8 @@ export default NextAuth({
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID,
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
+            scope: ["identify", "email", "guilds"],
+            authorizationEndpoint: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
         })
     ],
     secret: process.env.NEXT_AUTH_SECRET,
