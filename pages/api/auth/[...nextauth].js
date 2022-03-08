0b35aa0dd56869ee3ccda2e1f63854ee7509d337
Vann <82990248+Stevanvincent@users.noreply.github.com>
2022-03-08 08:25:43 +0700

    add guild scope

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export default NextAuth({
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID,
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
-            authorizationEndpoint: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
+            authorization: `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=https%3A%2F%2Fovertunes.me%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=guilds%20email%20identify`
         })
     ],
     secret: process.env.NEXT_AUTH_SECRET,
